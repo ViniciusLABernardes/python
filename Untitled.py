59560a51ac39e4650b1554c66c4eca308ea657a5
Vinicius Leandro <111243990+ViniciusLABernardes@users.noreply.github.com>
2024-03-08 22:36:06 -0300

    aula_python_04

---
+++ Untitled.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## nome
+# ## idade
+# ## peso
+# ## altura
+# ## usuario python
+# ## endereço
+# ## PCD
+# ## Data de nascimento
+
+# 
+
+# In[4]:
+
+
+nome = "vinicius"
+idade = 20
+peso = 92
+altura =1.91
+usuario_python = True
+endereco = "avenida ablablabla 123"
+pcd = False
+dia = 11
+mes = 9
+ano = 2001
+
+print(type(nome), f"nome: " , nome)
+print(f"nome: ",type(idade))
+print(f"nome: ",type(peso))
+print(f"nome: ",type(altura))
+print(type(usuario_python))
+print(type(endereco))
+print(type(pcd))
+print(type(dia))
+print(type(mes))
+print(type(ano))
+endereco.split(',')
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
